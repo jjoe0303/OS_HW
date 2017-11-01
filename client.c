de8ee3b008be134c7dde854a90f4fb21568d8c6b
F74042010 <jjoe0303@gmail.com>
2017-11-01 13:01:41 +0800

    User Interface Modified

--- client.c
+++ client.c
@@ -3,8 +3,27 @@
 int main(int argc, char **argv)
 {
 // write someting here...
-
-
+    char work;
+    int EXIT = 0;
+    while(!EXIT){
+        /****************************
+                User Interface
+        ****************************/
+        printf("======================================================");
+        printf("\n(a)list all process ids\n");
+        printf("(b)thread's IDs\n");
+        printf("(c)child's PIDs\n");
+        printf("(d)process name\n");
+        printf("(e)state of process(D,R,S,T,t,W,X,Z)\n");
+        printf("(f)command line of excuting process(cmdline)\n");
+        printf("(g)parent's PID\n");
+        printf("(h)all ancients of PIDs\n");
+        printf("(i)virtual memory size(VmSize)\n");
+        printf("(j)physical memory size(VmRSS)\n");
+        printf("(k)exit\n");
+        printf("which?");
+        scanf("%c",&work);
+    }
 	return 0;
 }
 
--- client.h
+++ client.h
@@ -2,6 +2,8 @@
 #define CLIENT_H
 
 #include "protocol.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 #endif
