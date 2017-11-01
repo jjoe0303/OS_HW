05006810871d6c39ff25493e00faa7baf9a0a18d
F74042010 <jjoe0303@gmail.com>
2017-11-02 00:58:46 +0800

    Can filter the target in proc file but don't know how to get the num

--- server.c
+++ server.c
@@ -4,28 +4,31 @@ int main(int argc, char **argv)
 {
 // write someting here...
 	FILE *fin;
-	char buffer[2000];
+	char buffer[100];
 	char pid[50];
-	printf("pid?");
-	scanf("%s",&pid);
-	openFile(fin,pid,buffer);
+	while(1) {
+		printf("pid?");
+		scanf("%s",&pid);
+		openFile(fin,pid,buffer);
+	}
 	return 0;
-
 }
 
 void openFile(FILE *fin, char pid[],char buffer[])
 {
 	/*concat the address*/
-	char address[70];
+	char address[70]="";
 	strcat(address,"/proc/");
 	strcat(address,pid);
 	strcat(address,"/status");
-
+	//printf("%s\n",address);
 	/*open file*/
 	fin = fopen(address,"r");
 	while(fgets(buffer,100,fin)!= NULL) {
-		printf("%s",buffer);
+		if(buffer[0]=='P' && buffer[1]=='i' && buffer[2]=='d') {
+			printf("%s",buffer);
+		}
 	}
-
+	fclose(fin);
 }
 
