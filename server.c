4f4bc0e642dc5d7cd6313ced16a78ae9bd2d4a53
F74042010 <jjoe0303@gmail.com>
2017-11-02 00:35:33 +0800

    Test Open & Read /proc file

--- server.c
+++ server.c
@@ -3,8 +3,28 @@
 int main(int argc, char **argv)
 {
 // write someting here...
+    FILE *fin;
+    char buffer[2000];
+    char pid[50];
+    printf("pid?");
+    scanf("%s",&pid);
+    openFile(fin,pid,buffer);
 	return 0;
+
 }
 
+void openFile(FILE *fin , char pid[] ,char buffer[]){
+    /*concat the address*/
+    char address[70];
+    strcat(address,"/proc/");
+    strcat(address,pid);
+    strcat(address,"/status");
 
+    /*open file*/
+    fin = fopen(address,"r");
+    while(fgets(buffer,100,fin)!= NULL){
+             printf("%s",buffer);
+    }
+    
+}
 
--- server.h
+++ server.h
@@ -2,5 +2,11 @@
 #define SERVER_H
 
 #include "protocol.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
 
+void openFile(FILE *fin,char pid[],char buffer[]);
 #endif
